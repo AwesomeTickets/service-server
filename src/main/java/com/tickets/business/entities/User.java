f35bb8ac06760d96e26e1aa8b291a1f05ad63427
YifengWong <395049219@qq.com>
2017-03-26 18:11:32 +0800

    create web.xml and servlet.xml, and change thymleaf's logger

--- src/main/java/com/tickets/business/entities/User.java
+++ src/main/java/com/tickets/business/entities/User.java
@@ -33,8 +33,8 @@ public class User {
         this.username = username;
         this.password = password;
     }
-    
-	@Override
+
+    @Override
     public String toString() {
         return "User [id=" + id + ", username=" + username + ", password=" + password + "]";
     }
