3074405ea15fa30fff43cb68341c091579c9f6c4
Steven Liu <stevennl@foxmail.com>
2017-03-28 18:09:50 +0800

    Add RESTFul result entity

---
+++ src/main/java/com/tickets/web/controller/WebServiceController.java
@@ -0,0 +1,40 @@
+package com.tickets.web.controller;
+
+import com.tickets.business.entities.ErrorResult;
+import com.tickets.business.entities.RestResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Controller of RESTFul web service.
+ */
+@RestController
+@RequestMapping("/resource")
+public class WebServiceController {
+    private static final Logger LOG = LoggerFactory.getLogger("tickets");
+
+    @RequestMapping(path = "/testRest", method = RequestMethod.GET)
+    public RestResult testRest(HttpServletRequest request, HttpServletResponse response) {
+        LOG.info(request.getMethod() + " " + request.getRequestURI());
+        response.setStatus(200);
+        RestResult result = new RestResult();
+        result.put("username:", "tickets");
+        result.put("password:", "123456");
+        return result;
+    }
+
+    @RequestMapping(path = "/testError", method = RequestMethod.GET)
+    public RestResult testError(HttpServletRequest request, HttpServletResponse response) {
+        LOG.info(request.getMethod() + " " + request.getRequestURI());
+        response.setStatus(404);
+        return new ErrorResult("Oops! An error occurs.");
+    }
+
+}
