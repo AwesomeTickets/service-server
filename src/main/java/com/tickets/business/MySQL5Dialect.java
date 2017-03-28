a9577cd1f22a99db33850e8063995ac05960af67
Chuyang Liu <chuyang.s.liu@gmail.com>
2017-03-28 18:09:50 +0800

    Add RESTFul result entity

--- src/main/java/com/tickets/business/MySQL5Dialect.java
+++ src/main/java/com/tickets/business/MySQL5Dialect.java
@@ -3,15 +3,10 @@ package com.tickets.business;
 import org.hibernate.dialect.MySQL5InnoDBDialect;
 
 /**
- * Created by CrazeWong on 2017/3/27.
- * Set the connection dialect myself
+ * Database dialect.
  */
 public class MySQL5Dialect extends MySQL5InnoDBDialect {
 
-    /* (non-Javadoc)
-     * @see org.hibernate.dialect.MySQL5InnoDBDialect#getTableTypeString()
-     * For UTF-8
-     */
     @Override
     public String getTableTypeString() {
         return " ENGINE=InnoDB DEFAULT CHARSET=utf8";
