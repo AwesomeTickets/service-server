225504427d74c1f175c4b484f9a6f6ce20096233
YifengWong <395049219@qq.com>
2017-03-31 00:31:06 +0800

    finish base api, wait test

--- src/main/java/com/tickets/business/entities/repositories/CountryRepository.java
+++ src/main/java/com/tickets/business/entities/repositories/CountryRepository.java
@@ -4,10 +4,13 @@ import com.tickets.business.entities.Country;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * Created by CrazeWong on 2017/3/29.
  */
 @Repository
-public interface CountryRepository extends CrudRepository<Country, Long> {
+public interface CountryRepository extends CrudRepository<Country, Integer> {
     // TODO CountryRepository
+    List<Country> findByName(String name);
 }
--- src/main/java/com/tickets/business/entities/repositories/MovieHasStyleRepository.java
+++ src/main/java/com/tickets/business/entities/repositories/MovieHasStyleRepository.java
@@ -1,9 +1,13 @@
 package com.tickets.business.entities.repositories;
 
+import com.tickets.business.entities.Movie;
 import com.tickets.business.entities.MovieHasStyle;
 import com.tickets.business.entities.MovieHasStylePK;
+import com.tickets.business.entities.MovieStyle;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by CrazeWong on 2017/3/29.
@@ -11,4 +15,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MovieHasStyleRepository extends CrudRepository<MovieHasStyle, MovieHasStylePK>{
     // TODO MovieHasStyleRepository
+    List<MovieHasStyle> findByMovie(Movie movie);
+
+    List<MovieHasStyle> findByMovieStyle(MovieStyle movieStyle);
 }
