02854dd754123fe5e3a730096eb0b2a76df1ee34
Steven Liu <stevennl@foxmail.com>
2017-04-01 22:04:14 +0800

    Add moive service to get large poster

--- src/main/java/com/tickets/business/services/MovieService.java
+++ src/main/java/com/tickets/business/services/MovieService.java
@@ -23,12 +23,31 @@ public class MovieService {
         super();
     }
 
-    public List<Integer> getMovieByStatus(String status) {
-        return movieRepo.findByMovieStatus(status);
-    }
-
+    /**
+     * Return the movie entity whose ID equals to @param movieID.
+     */
     public Movie getMovie(Integer movieID) {
         return movieRepo.findOne(movieID);
     }
 
+    /**
+     * Return the list of ID of movie whose status equals to @param status.
+     */
+    public List<Integer> getMovieByStatus(String status) {
+        return movieRepo.findByMovieStatus(status);
+    }
+
+    /**
+     * Return the list of (ID, URI of large poster) tuple.
+     *
+     * @param maxCount Maximum size of the result list
+     */
+    public List<Object[]> getLargePoster(int maxCount) {
+        // TODO
+        ArrayList<Object[]> posters = new ArrayList<Object[]>();
+        posters.add(new Object[]{1, "https://raw.githubusercontent.com/AwesomeTickets/Dashboard/master/img/poster/large/1.png"});
+        posters.add(new Object[]{2, "https://raw.githubusercontent.com/AwesomeTickets/Dashboard/master/img/poster/large/2.png"});
+        posters.add(new Object[]{3, "https://raw.githubusercontent.com/AwesomeTickets/Dashboard/master/img/poster/large/3.png"});
+        return posters;
+    }
 }
