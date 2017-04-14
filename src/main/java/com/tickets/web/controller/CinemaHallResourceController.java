4a8feea996328664eac15ae7217197520c87c892
Steven Liu <stevennl@foxmail.com>
2017-04-14 10:32:51 +0800

    Add new resources controller

---
+++ src/main/java/com/tickets/web/controller/CinemaHallResourceController.java
@@ -0,0 +1,19 @@
+package com.tickets.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * RESTFul API of cinema hall resources.
+ */
+@RestController
+@RequestMapping("/resource/cinema_hall")
+public class CinemaHallResourceController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CinemaHallResourceController.class);
+
+    // TODO API
+}
