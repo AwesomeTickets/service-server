b78b3d261cffc994ee092ff823435d491537ecc4
Lin Shilu <1092246302@qq.com>
2017-04-15 15:35:51 +0800

    Add virtual data

--- src/main/java/com/tickets/web/controller/CinemaResourceController.java
+++ src/main/java/com/tickets/web/controller/CinemaResourceController.java
@@ -5,7 +5,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import com.tickets.web.controller.response.CollectionResponse;
+import com.tickets.web.controller.response.ErrorResponse;
+import com.tickets.web.controller.response.RestResponse;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 /**
  * RESTFul API of cinema resources.
  */
@@ -14,6 +22,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class CinemaResourceController {
 
     private static final Logger LOG = LoggerFactory.getLogger(CinemaResourceController.class);
+    
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+    public RestResponse getCinema(@PathVariable Integer id, HttpServletRequest request, HttpServletResponse response) {
+        LOG.info(request.getMethod() + " " + request.getRequestURI());
+        RestResponse res = new RestResponse();
+        res.put("cinemaID", 3);
+        res.put("name", "金逸珠江国际影城（大学城店）");
+        res.put("location", "番禺区大学城XXX铺");
+        return res;
+    }
 
-    // TODO API
 }
