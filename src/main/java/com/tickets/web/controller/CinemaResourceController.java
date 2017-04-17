56dbbf80abe0560b7f949b1090f65e65b9e98651
Steven Liu <stevennl@foxmail.com>
2017-04-17 18:40:46 +0800

    Delete conflict files

--- src/main/java/com/tickets/web/controller/CinemaResourceController.java
+++ src/main/java/com/tickets/web/controller/CinemaResourceController.java
@@ -1,15 +1,16 @@
 package com.tickets.web.controller;
 
-import com.tickets.business.entities.Cinema;
-import com.tickets.business.services.CinemaService;
-import com.tickets.web.controller.response.RestResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.tickets.web.controller.response.CollectionResponse;
+import com.tickets.web.controller.response.ErrorResponse;
+import com.tickets.web.controller.response.RestResponse;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,28 +22,15 @@ import javax.servlet.http.HttpServletResponse;
 @RequestMapping("/resource/cinema")
 public class CinemaResourceController {
 
-    @Autowired
-    private CinemaService cinemaService;
-
     private static final Logger LOG = LoggerFactory.getLogger(CinemaResourceController.class);
 
-    @RequestMapping(path = "/{cinemaID}", method = RequestMethod.GET)
-    public RestResponse getCinema(@PathVariable Integer cinemaID, HttpServletRequest request, HttpServletResponse response) {
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+    public RestResponse getCinema(@PathVariable Integer id, HttpServletRequest request, HttpServletResponse response) {
         LOG.info(request.getMethod() + " " + request.getRequestURI());
-        RestResponse result = new RestResponse();
-        Cinema cinema = cinemaService.getCinema(cinemaID);
-
-        if (cinema == null) {
-            response.setStatus(404);
-            return null;
-        }
-
-        result.put("cinemaID", cinema.getCinemaID());
-        result.put("name", cinema.getName());
-        result.put("location", cinema.getLocation());
-
-        response.setStatus(200);
-        return result;
+        RestResponse res = new RestResponse();
+        res.put("cinemaID", 3);
+        res.put("name", "金逸珠江国际影城（大学城店）");
+        res.put("location", "番禺区大学城XXX铺");
+        return res;
     }
-
 }
