f6f4b57f24bbc5b31979be61c6ecd0a3955b7ed4
Chuyang Liu <chuyang.s.liu@gmail.com>
2017-04-19 22:23:43 +0800

    Print stack trace in tthe exception handler

--- src/main/java/com/tickets/web/ExceptionHandler.java
+++ src/main/java/com/tickets/web/ExceptionHandler.java
@@ -6,15 +6,17 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 /**
- * A ExceptionHandler to return a html for all exception.
+ * An ExceptionHandler to return a html for all exception.
  */
 public class ExceptionHandler implements HandlerExceptionResolver {
 
-    @Override
-    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
-
+    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response,
+                                         Object handler, Exception ex) {
         response.setStatus(500);
+        ex.printStackTrace();
         return new ModelAndView("exception");
     }
+
 }
