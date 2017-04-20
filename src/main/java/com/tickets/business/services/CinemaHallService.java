eb092e3927b9e134172ad59b8df30f78fb51d176
Lin Shilu <1092246302@qq.com>
2017-04-20 08:49:25 +0800

    correct cinemahall and seat

--- src/main/java/com/tickets/business/services/CinemaHallService.java
+++ src/main/java/com/tickets/business/services/CinemaHallService.java
@@ -1,12 +1,13 @@
 package com.tickets.business.services;
 
 import com.tickets.business.entities.CinemaHall;
+import com.tickets.business.entities.Cinema;
 import com.tickets.business.entities.repositories.CinemaHallRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import java.util.List;
 
 @Service
 public class CinemaHallService {
@@ -24,6 +25,41 @@ public class CinemaHallService {
      */
     public CinemaHall getCinemaHall(Integer cinemaHallID) {
         return cinemaHallRepo.findOne(cinemaHallID);
+    }
+
+    public CinemaHall getWithoutSeatLayout(Integer cinemaHallID)
+    {
+        List<Object[]> list = cinemaHallRepo.findWithoutSeatLayout(cinemaHallID);
+
+        Object[] o = list.get(0);
+        Integer cinemaID = (Integer)o[1];
+        String name = (String )o[2];
+
+        Cinema cinema = new Cinema();
+        cinema.setCinemaID(cinemaID);
+
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setCinemaHallID(cinemaHallID);
+        cinemaHall.setCinema(cinema);
+        cinemaHall.setName(name);
+
+        return cinemaHall;
+    }
+
+    public CinemaHall getWithSeatLayout(Integer cinemaHallID)
+    {
+        
+        List<Object[]> list = cinemaHallRepo.findWithSeatLayout(cinemaHallID);
+
+        Object[] o = list.get(0);
+        Integer cinemaID = (Integer)o[0];
+        String seatLayout = (String)o[1];
+
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setCinemaHallID(cinemaHallID);
+        cinemaHall.setSeatLayout(seatLayout);
+
+        return cinemaHall;
     }
 
 }
