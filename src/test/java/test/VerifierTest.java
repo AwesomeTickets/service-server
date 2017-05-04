2b7567c50f5165697faf84855b0436260f91e30e
YifengWong <395049219@qq.com>
2017-05-04 23:48:22 +0800

    finish sms controller

--- src/test/java/test/VerifierTest.java
+++ src/test/java/test/VerifierTest.java
@@ -1,20 +1,18 @@
 package test;
 
-import com.awesometickets.util.Verifier;
+import com.awesometickets.business.services.SmsService;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
-@Ignore
 public class VerifierTest extends BaseTest {
 
     @Test
     public void testSendSmsCode() {
-        Assert.assertTrue(Verifier.getInstance().sendSmsCode("13538773281"));
+        Assert.assertTrue(SmsService.getInstance().sendSmsCode("15622743170"));
     }
 
     @Test
     public void testVerifySmsCode() {
-        Assert.assertTrue(Verifier.getInstance().verifySmsCode("13538773281", "491121"));
+        Assert.assertTrue(SmsService.getInstance().verifySmsCode("15622743170", "948595"));
     }
 }
