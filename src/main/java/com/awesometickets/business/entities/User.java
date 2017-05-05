aafa06cd6e6145e87b1c7e00ce421cf8ff0d75f8
Chuyang Liu <chuyang.s.liu@gmail.com>
2017-05-05 23:22:16 +0800

    Fix User table primary generation bug

--- src/main/java/com/awesometickets/business/entities/User.java
+++ src/main/java/com/awesometickets/business/entities/User.java
@@ -14,7 +14,7 @@ public class User {
     }
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     public Integer getUserId() {
         return userId;
