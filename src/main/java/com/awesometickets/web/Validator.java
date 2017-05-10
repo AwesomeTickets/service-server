f789c37b85b8f11877a89cad706d4a2d32e99db5
YifengWong <395049219@qq.com>
2017-05-10 21:22:24 +0800

    redo ticket api and add a validator

--- src/main/java/com/awesometickets/web/Validator.java
+++ src/main/java/com/awesometickets/web/Validator.java
@@ -1,37 +1,72 @@
 package com.awesometickets.web;
 
-import com.awesometickets.business.entities.User;
-import com.awesometickets.business.services.UserService;
-import com.awesometickets.util.PhoneNumUtil;
-import com.awesometickets.web.controller.response.ErrorResponse;
-import com.awesometickets.web.controller.response.ErrorStatus;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Validation of all request parameters.
  */
 @Component
 public class Validator {
+    /**
+     * China TELECOM phone number
+     * at 133,153,180,181,189,177,1700,173
+     * **/
+    private static final String CHINA_TELECOM_PATTERN = "(^1(33|53|7[37]|8[019])\\d{8}$)|(^1700\\d{7}$)";
 
-    @Autowired
-    private UserService userService;
+    /**
+     * China UNICOM phone number
+     * at 130,131,132,155,156,185,186,145,176,1707,1708,1709
+     * **/
+    private static final String CHINA_UNICOM_PATTERN = "(^1(3[0-2]|4[5]|5[56]|7[6]|8[56])\\d{8}$)|(^170[7-9]\\d{7}$)";
 
-    public int validateTicketParams(User user, String phoneNum, Integer[] seats) {
-        if (!PhoneNumUtil.isPhone(phoneNum)) {
-            return ErrorStatus.PHONE_INVALID_FORMAT;
-        }
-        if (seats.length > 8 || seats.length < 2 || seats.length % 2 != 0) {
-            return ErrorStatus.BAD_REQUEST;
-        }
-        Integer userId = userService.getUserByPhoneNum(phoneNum);
-        if (user == null) {
-            return ErrorStatus.;
-        }
+    /**
+     * China MOBILE phone number
+     * at 134,135,136,137,138,139,150,151,152,157,158,159,182,183,184,187,188,147,178,1705
+     *
+     **/
+    private static final String CHINA_MOBILE_PATTERN = "(^1(3[4-9]|4[7]|5[0-27-9]|7[8]|8[2-478])\\d{8}$)|(^1705\\d{7}$)";
 
-        user.setUserId(userId);
-        return -1;
+    /**
+     * All phone number
+     */
+    private static final String PHONE_PATTERN=new StringBuilder().append(CHINA_MOBILE_PATTERN)
+            .append("|")
+            .append(CHINA_TELECOM_PATTERN)
+            .append("|")
+            .append(CHINA_UNICOM_PATTERN)
+            .toString();
+
+    private static final Pattern isPhonePattern = Pattern.compile(PHONE_PATTERN);
+
+    /**
+     * Check if the phone number is valid.
+     *
+     * @param phoneNum The phone number
+     * @return True if the phone number is valid
+     */
+    public boolean checkPhoneNum(String phoneNum) {
+        boolean flag = false;
+        try {
+            Matcher m = isPhonePattern.matcher(phoneNum);
+            flag = m.matches();
+        } catch (Exception e) {
+            flag = false;
+        }
+        return flag;
     }
 
-    public int validateTicketParams(String phoneNum, String ticketCode)
+    /**
+     * Check the seats format.
+     * @param seats
+     * @return True if the seats array is valid
+     */
+    public boolean checkSeatsFormat(Integer[] seats) {
+        return !(seats.length > 8 || seats.length < 2 || seats.length % 2 != 0);
+    }
+
+
+
 }
