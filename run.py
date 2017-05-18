252734aeaf1b22bb4a01543cdcf4f0ffdc80b590
Chuyang Liu <chuyang.liu@outlook.com>
2017-05-18 21:47:27 +0800

    Update scripts: merge server.py and deploy.py

---
+++ run.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+test = False
+background = False
+
+for arg in sys.argv[1:]:
+    if arg == '-t':
+        test = True
+    elif arg == '-b':
+        background = True
+
+if test:
+    os.system("mvn clean test")
+elif background:
+    try:
+        os.remove("nohup.out")
+    except Exception as e:
+        pass
+    os.system("nohup mvn clean tomcat7:run-war &")
+else:
+    os.system("mvn clean compile tomcat7:run")
