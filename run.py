2dc6e9fc39338be44152f7cb55ed99f4b924682e
Chuyang Liu <chuyang.s.liu@gmail.com>
2017-05-19 21:53:54 +0800

    Update run.py: add urandom var

--- run.py
+++ run.py
@@ -6,20 +6,33 @@ import sys
 
 test = False
 background = False
+var_urand = ''
+var_skip_test = '-Dmaven.test.skip=true '
 
 for arg in sys.argv[1:]:
     if arg == '-t':
         test = True
     elif arg == '-b':
         background = True
+    elif arg == '-u':
+        # http://nobodyiam.com/2016/06/07/tomcat-startup-slow/
+        var_urand = '-Djava.security.egd=file:/dev/./urandom '
+
+cmd_test = 'mvn clean test'
+cmd_background = 'nohup mvn clean tomcat7:run-war %s &' % \
+    (var_urand + var_skip_test)
+cmd_foreground = 'mvn clean tomcat7:run %s' % (var_urand)
 
 if test:
-    os.system("mvn clean test")
+    print("exec: " + cmd_test)
+    os.system(cmd_test)
 elif background:
     try:
         os.remove("nohup.out")
     except Exception as e:
         pass
-    os.system("nohup mvn -Dmaven.test.skip=true clean tomcat7:run-war &")
+    print("exec: " + cmd_background)
+    os.system(cmd_background)
 else:
-    os.system("mvn clean tomcat7:run")
+    print("exec: " + cmd_foreground)
+    os.system(cmd_foreground)
