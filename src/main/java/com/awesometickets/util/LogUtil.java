c7cb71ff3d1577fd98bf3d8aaa6183d7d46d7838
Steven Liu <stevennl@foxmail.com>
2017-05-19 16:23:14 +0800

    Update configs

--- src/main/java/com/awesometickets/util/LogUtil.java
+++ src/main/java/com/awesometickets/util/LogUtil.java
@@ -3,6 +3,8 @@ package com.awesometickets.util;
 import org.slf4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 
 public class LogUtil {
@@ -15,6 +17,11 @@ public class LogUtil {
         if (queryStr == null || queryStr.length() == 0) {
             logger.info(req.getMethod() + " " + req.getRequestURI());
         } else {
+            try {
+                queryStr = URLDecoder.decode(queryStr, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                logger.warn(e.getMessage());
+            }
             logger.info(req.getMethod() + " " + req.getRequestURI() + "?" + queryStr);
         }
     }
