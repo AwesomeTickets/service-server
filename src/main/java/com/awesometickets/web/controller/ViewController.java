43627c0694bcecde788bc1aa0f34cfa2509c71d8
Steven Liu <stevennl@foxmail.com>
2017-05-20 16:48:38 +0800

    Remove thymeleaf view engine

--- src/main/java/com/awesometickets/web/controller/ViewController.java
+++ src/main/java/com/awesometickets/web/controller/ViewController.java
@@ -6,15 +6,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-/**
- * Controller that returns views.
- */
-@Controller
+@RestController
 public class ViewController {
 
     private static final Logger Log = LoggerFactory.getLogger(ViewController.class);
@@ -26,19 +24,6 @@ public class ViewController {
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String index(HttpServletRequest request, HttpServletResponse response) {
         LogUtil.logReq(Log, request);
-        return "index";
+        return "Welcome to AwesomeTickets";
     }
-
-    @RequestMapping(path = "/test", method = RequestMethod.GET)
-    public String test(HttpServletRequest request, HttpServletResponse response) {
-        LogUtil.logReq(Log, request);
-        return "test";
-    }
-
-    @RequestMapping(path = "/tickets", method = RequestMethod.GET)
-    public String tickets(HttpServletRequest request, HttpServletResponse response) {
-        LogUtil.logReq(Log, request);
-        return "tickets";
-    }
-
 }
